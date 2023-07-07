68d0037a27cd4c8a7176dd963119b6f5ca88ccff
Diego Menegazzi <omenegazzi@gmail.com>
2023-07-07 20:47:11 -0300

    Corrigido problemas com pacotes do Model que estava em maiusculo.

---
+++ src/Model/entidades.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author murilo.comim
+ */
+public class entidades {
+    
+    private int id;
+    private int idCidade;
+    private String nome;
+    private String endereco;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCidade() {
+        return idCidade;
+    }
+
+    public void setIdCidade(int idCidade) {
+        this.idCidade = idCidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+    @Override
+    public String toString(){
+        return getNome();
+    }
+}
