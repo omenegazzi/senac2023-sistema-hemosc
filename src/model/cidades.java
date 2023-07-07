4f13120b5727987055706ae7856d8e27af3e9dc9
Murilo <murilo.comim@alunos.sc.senac.br>
2023-07-07 19:51:19 -0300

    Codigo do Model cidades concluido

--- src/model/cidades.java
+++ src/model/cidades.java
@@ -10,4 +10,45 @@ package model;
  */
 public class cidades {
     
+    private int id;
+    private int codigoIbge;
+    private String descricao;
+    private String uf;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCodigoIbge() {
+        return codigoIbge;
+    }
+
+    public void setCodigoIbge(int codigoIbge) {
+        this.codigoIbge = codigoIbge;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+    
+    @Override
+    public String toString(){
+        return getDescricao();
+    }
 }
