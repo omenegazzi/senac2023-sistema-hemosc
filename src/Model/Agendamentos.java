f039470d30836613656594b3f128127b1bb48f0d
Murilo <murilo.comim@alunos.sc.senac.br>
2023-07-07 20:58:02 -0300

    Criação do Model Agendamentos concluido

---
+++ src/Model/Agendamentos.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author murilo.comim
+ */
+public class Agendamentos {
+    
+    private int id;
+    private int data;
+    private int hora;
+    private int idDoador;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getHora() {
+        return hora;
+    }
+
+    public void setHora(int hora) {
+        this.hora = hora;
+    }
+
+    public int getIdDoador() {
+        return idDoador;
+    }
+
+    public void setIdDoador(int idDoador) {
+        this.idDoador = idDoador;
+    }
+    
+    
+}
