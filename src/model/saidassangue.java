fefcccead9cd4cc156470305b9e976965e19e27e
Murilo <murilo.comim@alunos.sc.senac.br>
2023-07-07 20:18:56 -0300

    Criação do Model SaidasSangue concluido

---
+++ src/model/saidassangue.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author murilo.comim
+ */
+public class saidassangue {
+    private int id;
+    private int idEntidade;
+    private int idTipoSanguineo;
+    private int data;
+    private String quantidade;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdEntidade() {
+        return idEntidade;
+    }
+
+    public void setIdEntidade(int idEntidade) {
+        this.idEntidade = idEntidade;
+    }
+
+    public int getIdTipoSanguineo() {
+        return idTipoSanguineo;
+    }
+
+    public void setIdTipoSanguineo(int idTipoSanguineo) {
+        this.idTipoSanguineo = idTipoSanguineo;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public String getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(String quantidade) {
+        this.quantidade = quantidade;
+    }
+    
+}
